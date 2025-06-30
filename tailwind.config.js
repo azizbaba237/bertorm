8679f41da1392c5ca6915e203e6fae8c97c17ad7
azizbaba237 <abdoulazizbaba227@gmail.com>
2025-06-30 15:47:20 +0100

    Fix static files and Tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/templates/**/*.html',
+    './static/src/**/*.js',
+    './**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
